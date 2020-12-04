eea61a056809b53e353a4edbc7803dfcc3a0905c
switchgirl95 <a.fokunang@gmail.com>
2020-12-04 06:17:54 +0100

    added necessary files

---
+++ twitter.py
@@ -0,0 +1,31 @@
+import tweepy
+import time
+import os
+from os import environ
+
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
+
+auth.set_access_token(ACCESS_KEY,ACCESS_SECRET)
+
+api = tweepy.API(auth, wait_on_rate_limit =True, wait_on_rate_limit_notify=True)
+
+user = api.me()
+
+search = '#reigen'
+nrTweets = 10
+
+for tweet in tweepy.Cursor(api.search, q="#reigen OR #reigenredraw OR #redrawreigen")
+#for tweet in tweepy.Cursor(api.search, search).items(nrTweets):
+	try:
+		#print('Tweet Liked')
+		tweet.retweet()
+		time.sleep(15)
+	except tweepy.TweepError as e:
+		print(e.reason)
+	except StopIteration:
+		break
